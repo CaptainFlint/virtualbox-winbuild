e9fcf702774c99b22793b76ab3b71493f4868857
Konstantin Vlasov <support@flint-inc.ru>
2019-01-17 16:08:02 +0300

    Added build configuration files and auxiliary scripts

---
+++ signer_client.py
@@ -0,0 +1,363 @@
+#!/usr/bin/python
+
+import sys
+import os
+import re
+import copy
+import argparse
+import uuid
+import httplib
+import time
+from datetime import datetime
+
+if sys.platform == 'win32':
+    import msvcrt
+else:
+    import fcntl
+
+
+__all__ = ['sign', 'sign_impl', 'getcerts']
+
+################################################################################
+# Global configuration
+
+# Command line arguments
+cmdlineArgs = {}
+# Signer server URL
+signerAddr = '10.28.28.192:8765'
+# URL of the SHA-1 timestamp server - as of May 2021, I could find no server still supporting SHA-1
+tsURL_sha1 = 'INVALID'
+# URL of the SHA-256 timestamp server
+tsURL_sha2 = 'http://timestamp.entrust.net/TSS/RFC3161sha2TS'
+
+# Alternative URLs
+# Globalsign:
+#tsURL_sha1 = 'http://timestamp.globalsign.com/scripts/timestamp.dll'
+#tsURL_sha2 = 'http://rfc3161timestamp.globalsign.com/advanced'
+# Digicert:
+#tsURL_sha1 = 'http://timestamp.digicert.com/'
+#tsURL_sha2 = 'http://timestamp.digicert.com/'
+
+# Lock file and its handle
+lockfile = re.sub(r'\.py.$', '.py', __file__) + '.lock'
+lockfh = None
+
+
+################################################################################
+# Auxiliary functions
+
+# If debug option is on, prints a message into stdout ensuring it will appear immediately
+# Input:
+#   msg - text message to print
+# Output:
+#   None
+def dbg_print(msg):
+    global cmdlineArgs
+    if not cmdlineArgs.debug:
+        return
+    sys.stdout.flush()
+    print msg
+    sys.stdout.flush()
+
+# Starts critical section for multiprocess sync; waits infinitely until the lock is acquired
+# Input:
+#   filepath - file name being processed in the CS (used for debugging prints only)
+# Output:
+#   None
+def cs_enter(filepath):
+    global cmdlineArgs, lockfile, lockfh
+    if cmdlineArgs.skip_lock:
+        return
+    dbg_print('SIGNLOCK: cs_enter on file %s' % (filepath))
+    lockfh = open(lockfile, 'w')
+    while True:
+        try:
+            if sys.platform == 'win32':
+                msvcrt.locking(lockfh.fileno(), msvcrt.LK_NBRLCK, 1)
+            else:
+                fcntl.flock(lockfh, fcntl.LOCK_EX | fcntl.LOCK_NB)
+            dbg_print('SIGNLOCK: lock acquired on file %s' % (filepath))
+            break
+        except IOError:
+            time.sleep(1)
+
+# Exits the critical section
+# Input:
+#   filepath - file name being processed in the CS (used for debugging prints only)
+# Output:
+#   None
+def cs_leave(filepath):
+    global cmdlineArgs, lockfile, lockfh
+    if cmdlineArgs.skip_lock:
+        return
+    dbg_print('SIGNLOCK: cs_leave on file %s' % (filepath))
+    if sys.platform == 'win32':
+        msvcrt.locking(lockfh.fileno(), msvcrt.LK_UNLCK, 1)
+    else:
+        fcntl.flock(lockfh, fcntl.LOCK_UN)
+    dbg_print('SIGNLOCK: unlocked file %s' % (filepath))
+    lockfh.close()
+    dbg_print('SIGNLOCK: closed lock on file %s' % (filepath))
+    lockfh = None
+
+# Creates HTTP POST request body from the specified fields.
+# Input:
+#   fields   - dictionary of fieldname:value for string values
+#   files    - dictionary of fieldname:path-to-file for files
+#   boundary - string used as multipart delimiter
+# Output:
+#   Formatted request body contents.
+def encode_multipart_formdata(fields, files, boundary):
+    lines = []
+    for (fieldname, value) in fields.iteritems():
+        lines.append('--' + boundary)
+        lines.append('Content-Disposition: form-data; name="%s"' % (fieldname))
+        lines.append('')
+        lines.append(value)
+    for (fieldname, filepath) in files.iteritems():
+        # Normally we work with small files, so keeping the full request body in memory is not a problem.
+        # If large files support is required, all this needs to be redesigned to use streamed read-write.
+        lines.append('--' + boundary)
+        lines.append('Content-Disposition: form-data; name="%s"; filename="%s"' % (fieldname, os.path.basename(filepath)))
+        lines.append('Content-Type: application/octet-stream')
+        lines.append('')
+        lines.append(open(filepath, 'rb').read())
+    lines.append('--' + boundary + '--')
+    lines.append('')
+    return '\r\n'.join(lines)
+
+################################################################################
+# Exported functions
+
+# User-friendly implementation for signing a file.
+# It adds the timestamping using the default predefined URLs. Otherwise, it's
+# the same as sign_impl().
+# Input:
+#   basic_params - (list) sequence of dicts, each with parameters for single signing:
+#       cert     - (str) thumbprint of the certificate to sign with
+#       passwd   - (str) password for the selected certificate's token
+#       hash     - (str) hash algorithm for signature (sha1 or sha2)
+#       tsurl    - (str, optional) timestamp server URL, or 'none' for no timestamping
+#       args     - (list) list of additional arguments for signtool
+#       [REMOVED] cross    - (bool) whether to use cross-certificate
+#   filepath     - (str) path to the file to be signed, will be replaced
+# Output:
+#   True if signing was successful; False otherwise. Error message (if any) is
+#   printed to standard output.
+def sign(basic_params, filepath):
+    sign_params = copy.deepcopy(basic_params)
+    for sp in sign_params:
+        if 'tsurl' not in sp or not sp['tsurl']:
+            if sp['hash'] == 'sha1':
+                sp['tsurl'] = tsURL_sha1
+            else:
+                sp['tsurl'] = tsURL_sha2
+    return sign_impl(sign_params, filepath)
+
+# Low-level signing function that represents a front-end for the HTML form procided by the signer server.
+# Input:
+#   sign_params - (list) sequence of dicts, each with parameters for single signing:
+#       cert    - (str) thumbprint of the certificate to sign with
+#       passwd  - (str) password for the selected certificate's token
+#       hash    - (str) hash algorithm for signature (sha1 or sha2)
+#       tsurl   - (str) timestamp server URL, or 'none' for no timestamping
+#       args    - (list) additional list of arguments for signtool
+#       [REMOVED] cross   - (bool) whether to use cross-certificate
+#   filepath    - (str) path to the file to be signed, will be replaced
+# Output:
+#   True if signing was successful; False otherwise. Error message (if any) is
+#   printed to standard output.
+def sign_impl(sign_params, filepath):
+    global cmdlineArgs
+    # Prepare list of fields for formatting multipart body
+    fields = {}
+    for i in range(len(sign_params)):
+        fields.update({
+            'cert%d'   % (i) : sign_params[i]['cert'],
+            'passwd%d' % (i) : sign_params[i]['passwd'],
+            'args%d'   % (i) : ' '.join(map(lambda s: ('"' + s + '"'), sign_params[i]['args'])),
+            #'cross%d'  % (i) : 'on' if sign_params[i]['cross'] else 'off',
+            'hash%d'   % (i) : sign_params[i]['hash']
+        })
+        if sign_params[i]['tsurl'] == 'none':
+            fields.update({
+                'tsopt%d' % (i) : 'none',
+                'tsurl%d' % (i) : ''
+            })
+        else:
+            fields.update({
+                'tsopt%d' % (i) : 'custom',
+                'tsurl%d' % (i) : sign_params[i]['tsurl']
+            })
+    files = {'filedata': filepath}
+
+    res = {'result': False, 'messages': []}
+    cs_enter(filepath)
+    try:
+        # Random boundary that will hardly appear in our data
+        boundary = '----------' + uuid.uuid4().hex
+        req_body = encode_multipart_formdata(fields, files, boundary)
+        conn = httplib.HTTPConnection(signerAddr, timeout = cmdlineArgs.timeout)
+        conn.request('POST', '/', req_body, {'Content-Type': 'multipart/form-data; boundary=%s' % (boundary)})
+        response = conn.getresponse()
+        if response.status == httplib.OK:
+            # File signed, save it under the same name overwriting the original
+            with open(filepath, 'wb') as f:
+                f.write(response.read())
+            res['messages'].append('File %s was signed successfully.' % (filepath))
+            res['result'] = True
+        else:
+            # Something went wrong; print what server has to tell about this
+            data = response.read()
+            res['messages'].append('%s %s' % (response.status, response.reason))
+            res['messages'].append(data)
+        conn.close()
+    except IOError, e:
+        res['messages'].append("File input/output problem: %s" % (e.strerror))
+    except httplib.HTTPException, e:
+        res['messages'].append("Connection problem: %s" % (e))
+    cs_leave(filepath)
+    return res
+
+# Lists all available certificates from the signer server.
+# Input:
+#   None
+# Output:
+#   List of dicts describing each certificate:
+#       name       - (str) subject name
+#       hash       - (str) hash algorithm
+#       thumbprint - (str) thumbprint
+#       expiry     - (str) expiration date/time in the format '<year>-<month>-<day>T<hours>:<minutes>:<seconds>'
+def getcerts():
+    global cmdlineArgs
+    conn = httplib.HTTPConnection(signerAddr, timeout = cmdlineArgs.timeout)
+    conn.request('GET', '/certlist')
+    response = conn.getresponse()
+    if response.status == httplib.OK:
+        res = {'result': [], 'messages': []}
+        re_cert_line = re.compile(r'^(\S+)\s+(.*)\s+\(([a-z0-9/]+)\)\s+\[(\d+-\d+-\d+T\d+:\d+:\d+)[^\[\]]*\]$', re.I)
+        for ln in response.read().split("\n"):
+            if ln == '':
+                continue
+            m = re_cert_line.match(ln)
+            if not m:
+                res['messages'].append("Failed to parse server output: '%s'" % (ln))
+                continue
+            res['result'].append({
+                'name': m.group(2),
+                'hash': m.group(3),
+                'thumbprint': m.group(1),
+                'expiry': datetime.strptime(m.group(4), '%Y-%m-%dT%H:%M:%S')
+            })
+    else:
+        # Something went wrong; print what server has to tell about this
+        res['messages'].append('Failed to obtain the list of certificates, server returned: %s %s' % (response.status, response.reason))
+        res['messages'].append(response.read())
+        res['result'] = None
+    conn.close()
+    return res
+
+
+################################################################################
+# Command line interface
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    subparsers = parser.add_subparsers(dest = 'cmd')
+
+    parser.add_argument('--timeout', action = 'store', default = 300, type = int,
+                       help = 'Timeout (in seconds) for HTTP operations')
+    parser.add_argument('--skip-lock', action = 'store_true',
+                       help = 'NOT RECOMMENDED! Allow multiple processes to send requests in parallel.')
+    parser.add_argument('--debug', action = 'store_true',
+                       help = 'Print some debugging information')
+
+    list_parser = subparsers.add_parser('list', help = 'List available certificates (thumbprint, name, hash algorithm)')
+
+    sign_parser = subparsers.add_parser('sign', help = 'Sign the file')
+
+    sign1 = sign_parser.add_argument_group('First signature')
+    sign1.add_argument('--tcert1', action = 'store', required = True,
+                       help = 'Thumbprint of the certificate to use')
+    sign1.add_argument('--passwd1', action = 'store', required = True,
+                       help = 'Token password for the certificate')
+    #sign1.add_argument('--cross1', action = 'store_true',
+    #                   help = 'Sign with cross-certificate')
+    sign1.add_argument('--hash1', action = 'store', choices = ['sha1', 'sha2'],
+                       help = 'Algorithm for the signature; default: sha2 for single-sign, sha1 for dual-sign')
+    sign1.add_argument('--tsurl1', action = 'store', required = False,
+                       help = "Timestamping server URL, or 'none' for no timestamping; default: Digicert")
+    sign1.add_argument('--args1', action = 'append',
+                       help = 'Additional arguments (can be supplied multiple times)')
+
+    sign2 = sign_parser.add_argument_group('Second signature (for dual-signing)')
+    sign2.add_argument('--tcert2', action = 'store',
+                       help = 'Thumbprint of the certificate to use')
+    sign2.add_argument('--passwd2', action = 'store',
+                       help = 'Token password for the certificate')
+    #sign2.add_argument('--cross2', action = 'store_true',
+    #                   help = 'Sign with cross-certificate')
+    sign2.add_argument('--hash2', action = 'store', choices = ['sha1', 'sha2'], default = 'sha2',
+                       help = 'Algorithm for the signature; default: sha2')
+    sign2.add_argument('--tsurl2', action = 'store', required = False,
+                       help = "Timestamping server URL, or 'none' for no timestamping; default: Digicert")
+    sign2.add_argument('--args2', action = 'append',
+                       help = 'Additional arguments (can be supplied multiple times)')
+
+    sign_parser.add_argument('file', nargs = 1,
+                             help = 'File to sign (will be replaced)')
+
+    cmdlineArgs = parser.parse_args()
+    if cmdlineArgs.cmd == 'list':
+        res = getcerts()
+        for msg in res['messages']:
+            print msg
+        if res['result']:
+            for c in res['result']:
+                c['expiry_fmt'] = c['expiry'].strftime('%d.%m.%Y %H:%M:%S UTC')
+                c['expired'] = ''
+                if c['expiry'] < datetime.utcnow():
+                    c['expired'] = '[X] '
+                print '%(expired)s%(thumbprint)s %(name)s (%(hash)s), valid till: %(expiry_fmt)s' % (c)
+    else:
+        # Configure the conditional default for hash1
+        if not getattr(cmdlineArgs, 'hash1', None):
+            if getattr(cmdlineArgs, 'tcert2', None):
+                cmdlineArgs.hash1 = 'sha1'
+            else:
+                cmdlineArgs.hash1 = 'sha2'
+        # Prepare parameters for sign()
+        basic_params = []
+        n = 1
+        while True:
+            s = {
+                'cert'   : getattr(cmdlineArgs, 'tcert%d'  % (n), None),
+                'passwd' : getattr(cmdlineArgs, 'passwd%d' % (n), None),
+                #'cross'  : getattr(cmdlineArgs, 'cross%d'  % (n), False),
+                'hash'   : getattr(cmdlineArgs, 'hash%d'   % (n), None),
+                'tsurl'  : getattr(cmdlineArgs, 'tsurl%d'  % (n), None),
+                'args'   : getattr(cmdlineArgs, 'args%d'   % (n), None)
+            }
+            if not s['cert']:
+                break
+            if not s['passwd']:
+                print 'Password for certificate No.%d cannot be empty.' % (n)
+                exit(1)
+            if not s['args']:
+                s['args'] = []
+            basic_params.append(s)
+            n += 1
+
+        res = sign(basic_params, cmdlineArgs.file[0])
+        for msg in res['messages']:
+            print msg
+
+    exit(0 if res['result'] else 1)
+else:
+    # Emulate required command line arguments with their default values
+    class PseudoArgParse:
+        def __init__(self, *args, **kwargs):
+            self.timeout = 300
+            self.skip_lock = False
+            self.debug = False
+    cmdlineArgs = PseudoArgParse()
